ddbfa3846282e7d8e4ca331006057a81e9e29cb7
ZhekaProff <Zhekakad@ya.ru>
2022-05-20 12:05:16 +0900

    решение четвертой задачи

---
+++ Task04/Program.cs
@@ -0,0 +1,9 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("Введите число а: ");
+int a =  int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите нужную степень: ");
+int b =  int.Parse(Console.ReadLine() ?? "0");
+int summa = 1;
+for ( int i = 1; i <= b; i++) 
+     summa = summa * a;    
+Console.WriteLine($"С {summa}");
