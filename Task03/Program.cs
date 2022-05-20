333b72f06ec34a88f71d325c2bbed9261e5ac5a9
ZhekaProff <Zhekakad@ya.ru>
2022-05-20 11:31:16 +0900

    решение третьей задачи

---
+++ Task03/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму чисел от 1 до А
+Console.WriteLine("Введите число: ");
+int NumberA =  int.Parse(Console.ReadLine() ?? "0");
+int summa = 0;
+for (int i = 0; i <= NumberA; i++)
+    summa =  summa + i;
+
+Console.WriteLine($"Сумма чисел от 1 до {NumberA} равна {summa}");
