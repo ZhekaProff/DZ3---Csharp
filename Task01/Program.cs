dba0c6a22b14f0ba20e6cff4ef1cfdc33edd1162
ZhekaProff <Zhekakad@ya.ru>
2022-05-20 12:09:14 +0900

    решение первой задачи

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Показать таблицу квадратов чисел от 1 до N
 Console.WriteLine("Введите число: ");
-int n = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine() ?? "0");
 for (int i = 1; i <=n; i++)
 {
     Console.Write($"{i * i} ");
