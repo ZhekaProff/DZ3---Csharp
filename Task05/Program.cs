308dfa05aa790c08f3c9ca1f6b760b42b05bdea9
ZhekaProff <Zhekakad@ya.ru>
2022-05-20 12:28:00 +0900

    решение пятой задачи

---
+++ Task05/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int count =  int.Parse(Console.ReadLine() ?? "0");
+int index = 0;
+while (count > 0)
+{
+    int digit = count % 10;
+    index = index + digit;
+    count = count / 10;
+}
+Console.WriteLine($" Сумма числа = {index}");
