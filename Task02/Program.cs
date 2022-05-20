3e7b72dec919e17761295d0aafbb277cb1618dd4
ZhekaProff <103837342+ZhekaProff@users.noreply.github.com>
2022-05-20 11:32:10 +0900

    Update Program.cs

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Найти кубы чисел от 1 до N
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите число: ") ?? "0");
 int n = int.Parse(Console.ReadLine());
 for (int i = 1; i <=n; i++)
 {
