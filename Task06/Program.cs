4826daf93b5fc2e539486eea9e0729e1fb1930a8
ZhekaProff <Zhekakad@ya.ru>
2022-05-20 12:36:44 +0900

    решение шестой задачи

---
+++ Task06/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i <=n; i++)
+{
+    if ( i % 2 == 0)
+    Console.Write($"{i * i * i} ");
+}
